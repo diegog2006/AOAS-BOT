30dfaea19d10f277050363e449bbeb70560e26cc
Lucky_Bot <87086889+Lucky-Cv@users.noreply.github.com>
2022-08-18 23:40:39 +0200

    Update info-creadoro.js

--- plugins/info-creadoro.js
+++ plugins/info-creadoro.js
@@ -3,34 +3,33 @@ let pp = 'https://i.imgur.com/lkch77g.jpeg'
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let texto1 = `
-┌──「𓈃ּ ៹🍏𝑯𝒂𝒅𝒆𝒔𐂂!s⁹⁷☻‹𝟹♞ᵇᵒᵗ⁻ᴹᴰ」─
+┌──「𓈃ּLucky ᵇᵒᵗ⁻ᴹᴰ」─
 ├❏𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩
-├❏Wa.me/5212411347467 (BOT)
-├❏Wa.me/5212411261883 (BOT) 
-├❏Wa.me/5212411719888 (CREADOR)
-├❏Wa.me/5212411347465 (CREADOR)
+├❏Wa.me/34641614021 (BOT) 
+├❏Wa.me/34643694252 (CREADOR)
+├❏Wa.me/15023065534 (CREADOR)
 └────ׂ─ׂ─ׂ─ׂ───
 ㅤ`
 let buttonMessage= {
-'document': { url: `http://wa.me/5212411347465` },
+'document': { url: `http://wa.me/34643694252` },
 'mimetype': `application/${document}`,
-'fileName': `𝚃𝚑𝚎 𝙷𝙰𝙳𝙴𝚂 𝙱𝙾𝚃`,
+'fileName': `𝚃𝚑𝚎 LUCKY 𝙱𝙾𝚃`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'http://wa.me/5212411347465',
+'mediaUrl': 'http://wa.me/31643694252',
 'mediaType': 2,
 'previewType': 'pdf',
-'title': 'http://wa.me/5212411347465⁩',
+'title': 'http://wa.me/34643694252',
 'body': wm,
  }},
 'caption': texto1,
 'footer': wm,
 'buttons':[
-{buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸'}, type: 1}],
+{buttonId: `${usedPrefix}menu`, buttonText: {displayText: 'LUCKY OFC'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
 handler.command = /^(contacto|owner|creator|propietario|dueña|dueño|propietaria|dueño|creadora|creador)$/i
